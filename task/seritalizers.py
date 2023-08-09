910f62cc6b868a97aff5fc451642e9d732a8471c
Dimitri Edel <dimitri.edel@gmx.net>
2023-08-09 20:48:25 +0200

    Add is_owner field to task.serializers.TaskSerializer

--- task/seritalizers.py
+++ task/seritalizers.py
@@ -3,12 +3,22 @@ from .models import *
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username') 
-    
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        # The request object has been passed as a parameter to the constructor
+        # in the views
+        request = self.context['request']
+        # Return True if the user is the owner of the object
+        return request.user == obj.owner
+
+
     class Meta:
         model = Task
         fields = [
             'id',
             'owner',
+            'is_owner',
             'asigned_to',
             'title',
             'comment',
