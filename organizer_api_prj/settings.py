b3929b35a214661ffe72c47aa39c9349c75c98e2
Dimitri Edel <dimitri.edel@gmx.net>
2023-08-27 02:24:59 +0200

    Alter hostname in ALLOWED_HOSTS

--- organizer_api_prj/settings.py
+++ organizer_api_prj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'organizer-api-f1f640e8d82c.herokuapp.com', 'https://organizer-second-826e64e1ee6e.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'organizer-api-f1f640e8d82c.herokuapp.com', 'organizer-second-826e64e1ee6e.herokuapp.com']
 
 # CLOUDINARY
 CLOUDINARY_STORAGE = {
